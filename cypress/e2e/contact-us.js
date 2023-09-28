b6455800be35d5d1dc7302c26fcd9cfe5695b361
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-09-28 09:43:06 +0200

    A BIT MORE

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -1,6 +1,10 @@
+/// <reference types="Cypress"/>
+
 describe("Test Contact Us page form viac webdriverUni", () => {
   it("Should be able to submit a successful submission via contact us form", () => {
     // possitive scenario code
+    cy.visit("https://webdriveruniversity.com/");
+    cy.get("#contact-us").click({ force: true });
   });
 
   it("Should not be able to submit a successful submission via contact us form ass all fields are required", () => {
