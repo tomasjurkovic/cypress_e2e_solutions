53bacfbc051b1671c6fd680fcce61711a97459db
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-09-28 09:46:25 +0200

    first type

--- cypress/e2e/contact-us.js
+++ cypress/e2e/contact-us.js
@@ -3,8 +3,12 @@
 describe("Test Contact Us page form viac webdriverUni", () => {
   it("Should be able to submit a successful submission via contact us form", () => {
     // possitive scenario code
-    cy.visit("https://webdriveruniversity.com/");
-    cy.get("#contact-us").click({ force: true });
+    // cy.visit("https://webdriveruniversity.com/");
+    // cy.get("#contact-us").click({ force: true });
+
+    // NOW JUST FAKE ACCESS THE OPENED TAB:
+    cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.get('[name="first_name"]').type("Tomas");
   });
 
   it("Should not be able to submit a successful submission via contact us form ass all fields are required", () => {
