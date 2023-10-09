2f1257b070fc6dd14b2f64baf5035b1ab19a49e6
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-09 10:38:45 +0200

    using promises in cypress code

--- cypress/e2e/automation-test-store/inspect-item.js
+++ cypress/e2e/automation-test-store/inspect-item.js
@@ -1,10 +1,16 @@
 /// <reference types="Cypress"/>
 
 describe("Inspect Automation test store using chain of commands", () => {
-  it("Click on the first item using item text", () => {
+  it.only("Click on the first item using item text", () => {
     cy.visit("https://automationteststore.com/");
-    cy.get(".prdocutname").contains("Skinsheen Bronzer Stick").click();
+    cy.get(".prdocutname")
+      .contains("Skinsheen Bronzer Stick")
+      .click()
+      .then(function (itemHeaderText) {
+        cy.log(`Selected the following item ${itemHeaderText.text()}`);
+      });
   });
+
   it("Click on the first item using index", () => {
     cy.visit("https://automationteststore.com/");
     cy.get(".fixed_wrapper").find(".prdocutname").eq(0).click(); // 0 retunrs fisrt element which it finds
