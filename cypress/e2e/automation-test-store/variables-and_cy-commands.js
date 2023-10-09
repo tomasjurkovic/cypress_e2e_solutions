3bd3a064f81d1944bb4bf7b292065b34966221a7
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-09 14:33:20 +0200

    finish course part

--- cypress/e2e/automation-test-store/variables-and_cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and_cy-commands.js
@@ -54,7 +54,7 @@ describe("Verifying variables, cypress commands and jquerry commands", () => {
     });
   });
 
-  it.only("Iterating over element", () => {
+  it("Iterating over element", () => {
     cy.visit("https://automationteststore.com/");
     cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
     cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
@@ -62,7 +62,7 @@ describe("Verifying variables, cypress commands and jquerry commands", () => {
     });
   });
 
-  it.only("Iterating over element", () => {
+  it("Iterating over element", () => {
     cy.visit("https://automationteststore.com/");
     cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
     cy.get(".fixed_wrapper .prdocutname").each(($el, index, $list) => {
