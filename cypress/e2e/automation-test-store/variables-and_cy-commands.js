71bed4d95791b8d9095c8b95b9ae17bd3183757e
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-09 11:23:42 +0200

    Update variables-and_cy-commands.js

--- cypress/e2e/automation-test-store/variables-and_cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and_cy-commands.js
@@ -29,4 +29,20 @@ describe("Verifying variables, cypress commands and jquerry commands", () => {
         expect(headerText).is.eq("Makeup");
       });
   });
+
+  it.only("Validate correct text names", () => {
+    cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+
+    // Using cypress commans and chaining:
+    cy.contains("#ContactUsFrm", "Contact Us Form")
+      .find("#field_11")
+      .should("contain", "First name");
+    // here I can check if it has specific ID and correct name in one placr:
+    // inside of it I found specific ID of a DIV
+    // and check that it contains 'First name' text label
+
+    // Using JQuerry:
+
+    // Embedded commands (closures):
+  });
 });
