46955d40db9a7c7a956c50db3ee6f3e89f171447
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-09 15:56:39 +0200

    finished section

--- cypress/e2e/automation-test-store/alias-invoke.js
+++ cypress/e2e/automation-test-store/alias-invoke.js
@@ -10,4 +10,14 @@ describe("Aliases and Invoke test suite", () => {
     cy.get("@productThumbnail").its("length").should("be.gt", 5);
     cy.get("@productThumbnail").should("include", "Seaweed Conditioner");
   });
+
+  it.only("Validate hair care products quantity and add to cart button", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get(".thumbnail").as("products");
+    cy.get("@products").should("have.length", 16);
+    cy.get("@products")
+      .find(".productcart")
+      .invoke("attr", "title")
+      .should("include", "Add to Cart");
+  });
 });
