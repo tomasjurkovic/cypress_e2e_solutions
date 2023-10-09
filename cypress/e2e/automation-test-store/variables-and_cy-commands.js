26b5a93dbc725ec222e22e22fc916b9404c9414b
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-09 11:14:32 +0200

    added new test using variables

--- cypress/e2e/automation-test-store/variables-and_cy-commands.js
+++ cypress/e2e/automation-test-store/variables-and_cy-commands.js
@@ -1,14 +1,32 @@
 describe("Verifying variables, cypress commands and jquerry commands", () => {
   it("Navigating to specific product pages", () => {
     cy.visit("https://automationteststore.com/");
-    const makeupLink = cy
-      .get("a[href*='product/category&path=']")
-      .contains("Makeup");
-    const skincareLink = cy
-      .get("a[href*='product/category&path=']")
-      .contains("Skincare");
 
-    makeupLink.click();
-    skincareLink.click();
+    // this will work:
+    // const makeupLink = cy
+    //   .get("a[href*='product/category&path=']")
+    //   .contains("Makeup");
+    // makeupLink.click();
+
+    // const skincareLink = cy
+    //   .get("a[href*='product/category&path=']")
+    //   .contains("Skincare");
+    //   skincareLink.click();
+
+    // recommended approach:
+    cy.get("a[href*='product/category&path=']").contains("Makeup").click();
+    cy.get("a[href*='product/category&path=']").contains("Skincare").click();
+  });
+
+  it("Navigating to specific product pages", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get("a[href*='product/category&path=']")
+      .contains("Makeup")
+      .click()
+      .then(($headerText) => {
+        const headerText = $headerText.text().trim();
+        cy.log(`Found header text: ${headerText}`);
+        expect(headerText).is.eq("Makeup");
+      });
   });
 });
