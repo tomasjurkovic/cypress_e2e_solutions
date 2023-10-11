cc0690af1f3324c2029dc9a65d61034c16d43641
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-11 10:07:25 +0200

    two tests for same origin policy added

---
+++ cypress/e2e/same-origin-policy/same-origin-policy.js
@@ -0,0 +1,11 @@
+describe("Cypress web security", () => {
+  it("Validate visiting two different domains", () => {
+    cy.visit("https://www.webdriveruniversity.com/");
+    cy.visit("https://automationteststore.com/");
+  });
+
+  it("Validate visiting two different domains via user actions", () => {
+    cy.visit("https://www.webdriveruniversity.com/");
+    cy.get("#automation-test-store").invoke("removeAttr", "target").click();
+  });
+});
