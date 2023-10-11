afb0292f0845650f2ccb145e4188aa79dccfc5d9
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-11 15:33:34 +0200

    finish checkbox section

--- cypress/e2e/webdriver-uni/checkboxes.js
+++ cypress/e2e/webdriver-uni/checkboxes.js
@@ -1,7 +1,7 @@
 const { outsmartNewTabOpening } = require("../../support/commands");
 
 describe("Handling checkboxes", () => {
-  it("Handle checking checkbox", () => {
+  it("Validate checking checkbox", () => {
     // possitive scenario code
     cy.visit("https://webdriveruniversity.com/");
 
@@ -15,7 +15,7 @@ describe("Handling checkboxes", () => {
     //cy.get('#checkboxes > :nth-child(1) input')
   });
 
-  it("Handle unchecking checkbox", () => {
+  it("Validate unchecking checkbox", () => {
     // possitive scenario code
     cy.visit("https://webdriveruniversity.com/");
 
@@ -26,5 +26,35 @@ describe("Handling checkboxes", () => {
     cy.get("#checkboxes input[value='option-3']").as("option3");
     cy.get("@option3").uncheck();
     cy.get("@option3").should("not.be.checked");
+  });
+
+  it.only("Validate checking multiple checkboxes at once", () => {
+    // possitive scenario code
+    cy.visit("https://webdriveruniversity.com/");
+
+    // using function for repeating step:
+    outsmartNewTabOpening("#dropdown-checkboxes-radiobuttons");
+
+    // checkbox 3 is already chekced
+    cy.get("#checkboxes input").as("checkboxes");
+    cy.get("@checkboxes").check();
+    cy.get("@checkboxes").should("be.checked");
+  });
+
+  it.only("Validate that some checkboxes are checked and some not", () => {
+    // possitive scenario code
+    cy.visit("https://webdriveruniversity.com/");
+
+    // using function for repeating step:
+    outsmartNewTabOpening("#dropdown-checkboxes-radiobuttons");
+
+    // checkbox 3 is already chekced
+    cy.get("#checkboxes input").check(["option-2", "option-4"]);
+
+    // checking that 1. checkbox is not checked and all other are:
+    cy.get("#checkboxes input[value='option-1']").should("not.be.checked");
+    cy.get("#checkboxes input[value='option-2']").should("be.checked");
+    cy.get("#checkboxes input[value='option-3']").should("be.checked");
+    cy.get("#checkboxes input[value='option-4']").should("be.checked");
   });
 });
