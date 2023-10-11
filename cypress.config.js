87080861a01696efee397a21c95711fb9e55b240
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-11 10:17:39 +0200

    added chrome web security to false

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,6 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
+    chromeWebSecurity: false,
   },
 });
