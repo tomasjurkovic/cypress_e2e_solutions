4f629642c14706762a7556f8519a38b4785446af
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-12 16:00:51 +0200

    added color checks and doubleclick event validation

--- cypress/e2e/webdriver-uni/mouse-actions.js
+++ cypress/e2e/webdriver-uni/mouse-actions.js
@@ -21,5 +21,27 @@ describe("Handling mouse action", () => {
       .trigger("mouseup", { force: true });
 
     cy.get("#droppable").invoke("text").should("include", "Dropped!");
+    cy.get("#droppable p")
+      .should("have.css", "background-color")
+      .and("eq", "rgb(244, 89, 80)");
+  });
+
+  it("It should be able to perform doubleclick event", () => {
+    cy.visit("https://webdriveruniversity.com/");
+    // using function for repeating step:t
+    outsmartNewTabOpening("#actions");
+    // check first color:
+    cy.get("#double-click")
+      .should("have.css", "background-color")
+      .and("eq", "rgb(254, 196, 45)");
+    cy.get("#double-click").dblclick();
+
+    // verify color-change after doubleclick:
+    cy.get("#double-click")
+      .should("have.css", "color")
+      .and("eq", "rgb(0, 0, 0)");
+    cy.get("#double-click")
+      .should("have.css", "background-color")
+      .and("eq", "rgb(147, 203, 90)");
   });
 });
