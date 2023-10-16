644a79cb9d19d9efffd1c041cbe25946aa0a70a6
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-16 13:33:24 +0200

    Update hooks.js

--- cypress/e2e/webdriver-uni/hooks.js
+++ cypress/e2e/webdriver-uni/hooks.js
@@ -1,17 +1,27 @@
 describe("Hooks", () => {
   before(() => {
-    // runs once before all tests in the block
+    cy.log("runs once before all tests in the block");
   });
 
   beforeEach(() => {
+    cy.log("runs once before each test in the block");
+
     // runs before each test in the block
   });
 
   afterEach(() => {
-    // runs after each test in the block
+    cy.log("runs after each test in the block");
   });
 
   after(() => {
-    // runs once after all tests in the block
+    cy.log("runs once after all tests in the block");
+  });
+
+  it("Example test 1", () => {
+    cy.log("Example test 1");
+  });
+
+  it("Example test 2", () => {
+    cy.log("Example test 2");
   });
 });
