9dfc851d842dfa5c87154dcf5c76a17f040281b7
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-16 13:38:26 +0200

    USING BEFORE EACH IN CHECKBOXES TESTS

--- cypress/e2e/webdriver-uni/checkboxes.js
+++ cypress/e2e/webdriver-uni/checkboxes.js
@@ -1,12 +1,14 @@
 const { outsmartNewTabOpening } = require("../../support/commands");
 
 describe("Handling checkboxes", () => {
-  it("Validate checking checkbox", () => {
-    // possitive scenario code
+  beforeEach(() => {
     cy.visit("https://webdriveruniversity.com/");
 
     // using function for repeating step:
     outsmartNewTabOpening("#dropdown-checkboxes-radiobuttons");
+  });
+
+  it("Validate checking checkbox", () => {
     // with assertion as well
     cy.get("#checkboxes input[value='option-1']").as("option1");
     cy.get("@option1").check();
@@ -16,38 +18,20 @@ describe("Handling checkboxes", () => {
   });
 
   it("Validate unchecking checkbox", () => {
-    // possitive scenario code
-    cy.visit("https://webdriveruniversity.com/");
-
-    // using function for repeating step:
-    outsmartNewTabOpening("#dropdown-checkboxes-radiobuttons");
-
     // checkbox 3 is already chekced
     cy.get("#checkboxes input[value='option-3']").as("option3");
     cy.get("@option3").uncheck();
     cy.get("@option3").should("not.be.checked");
   });
 
-  it.only("Validate checking multiple checkboxes at once", () => {
-    // possitive scenario code
-    cy.visit("https://webdriveruniversity.com/");
-
-    // using function for repeating step:
-    outsmartNewTabOpening("#dropdown-checkboxes-radiobuttons");
-
+  it("Validate checking multiple checkboxes at once", () => {
     // checkbox 3 is already chekced
     cy.get("#checkboxes input").as("checkboxes");
     cy.get("@checkboxes").check();
     cy.get("@checkboxes").should("be.checked");
   });
 
-  it.only("Validate that some checkboxes are checked and some not", () => {
-    // possitive scenario code
-    cy.visit("https://webdriveruniversity.com/");
-
-    // using function for repeating step:
-    outsmartNewTabOpening("#dropdown-checkboxes-radiobuttons");
-
+  it("Validate that some checkboxes are checked and some not", () => {
     // checkbox 3 is already chekced
     cy.get("#checkboxes input").check(["option-2", "option-4"]);
 
