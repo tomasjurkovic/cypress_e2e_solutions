a2f6e0265b20e439476e8e2bee8027794841e085
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-16 13:08:10 +0200

    added laptop.png file and two tests for uploading a file and neg. scenario

---
+++ cypress/e2e/webdriver-uni/file-upload.js
@@ -0,0 +1,24 @@
+const { outsmartNewTabOpening } = require("../../support/commands");
+
+describe("Handling uploading files", () => {
+  beforeEach(() => {
+    cy.visit("https://webdriveruniversity.com/");
+
+    // using function for repeating step:
+    outsmartNewTabOpening("#file-upload");
+  });
+  it("Validate file upload", () => {
+    cy.get("#myFile").selectFile("cypress/fixtures/example.json"); // providing real route to file
+    cy.get("#submit-button").click();
+    cy.on("window:alert", (str) => {
+      expect(str).to.include("Your file has now been uploaded!");
+    });
+  });
+  it("Validate no file upload", () => {
+    // negative scenario when clicking on submit button before file is attached
+    cy.get("#submit-button").click();
+    cy.on("window:alert", (str) => {
+      expect(str).to.include("You need to select a file to upload!");
+    });
+  });
+});
