4d1986a2d9930889722552e8a7d6339f0ab0eb53
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-16 11:20:30 +0200

    example of past date selecting

--- cypress/e2e/webdriver-uni/datepicker.js
+++ cypress/e2e/webdriver-uni/datepicker.js
@@ -25,7 +25,7 @@ describe("Handling radio buttons", () => {
     cy.get("#datepicker input").invoke("val").should("eq", dateInString);
   });
 
-  it.only("Selecting dates examples", () => {
+  it("Selecting future dates examples with assertion", () => {
     let date = new Date();
     date.setDate(date.getDate() + 365);
 
@@ -66,5 +66,48 @@ describe("Handling radio buttons", () => {
     cy.get("#datepicker input")
       .invoke("val")
       .should("eq", completeFutureDateInString);
+  });
+
+  it.only("Selecting past dates examples with assertion", () => {
+    let date = new Date();
+    date.setDate(date.getDate() - 365);
+
+    let pastYear = date.getFullYear();
+    let pastMonth = date.toLocaleDateString("default", { month: "long" });
+    let pastMonthNumber = date.getMonth() + 1; // it is index based for weird reason
+    let pastDay = date.getDate();
+    let completePastDateInString = `${pastMonthNumber}-${pastDay}-${pastYear}`;
+
+    cy.log(
+      `past year to select: ${pastYear}.\npast month to select: ${pastMonth} (${pastMonthNumber}).\npast day to select: ${pastDay}.`
+    );
+    cy.get("#datepicker").click();
+
+    function selectMonthAndYear() {
+      cy.get(".datepicker-dropdown")
+        .find(".datepicker-switch")
+        .first()
+        .then((currentDate) => {
+          if (!currentDate.text().includes(pastYear)) {
+            cy.get(".prev").first().click();
+            selectMonthAndYear(); // this will keep going as a loop until it matches criteria
+          }
+        })
+        .then((currentDate) => {
+          if (!currentDate.text().includes(pastMonth)) {
+            cy.get(".prev").first().click({ force: true });
+            selectMonthAndYear(); // this will keep going as a loop until it matches criteria
+          }
+        });
+    }
+
+    function selectPastDay() {
+      cy.get('[class="day"]').contains(pastDay).click();
+    }
+    selectMonthAndYear();
+    selectPastDay();
+    cy.get("#datepicker input")
+      .invoke("val")
+      .should("eq", completePastDateInString);
   });
 });
