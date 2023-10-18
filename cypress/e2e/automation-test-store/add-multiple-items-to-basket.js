f4223ce4b1b0ee481e2ce9eb3be5a0d2e4d5b879
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-18 15:51:55 +0200

    now it's working correctly

--- cypress/e2e/automation-test-store/add-multiple-items-to-basket.js
+++ cypress/e2e/automation-test-store/add-multiple-items-to-basket.js
@@ -10,9 +10,10 @@ describe("Adding multiple items to the basket", () => {
     cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
   });
 
-  it("Add specific items to the basket", () => {
+  it("Add specific items to basket", () => {
     globalThis.data.productNames.forEach(function (element) {
-      cy.addProductToBasket(data.productNames[element]);
+      cy.addProductToBasket(element);
     });
+    cy.get(".dropdown-toggle > .fa").click();
   });
 });
