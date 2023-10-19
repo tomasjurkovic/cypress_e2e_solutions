0250a726924fb6f1cc08b645370b0c0abd6aaf2f
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-19 16:08:11 +0200

    created same for automation test store home page

--- cypress/e2e/automation-test-store/add-multiple-items-to-basket.js
+++ cypress/e2e/automation-test-store/add-multiple-items-to-basket.js
@@ -1,4 +1,7 @@
+import AutoStore_Homepage_PO from "../../support/pageObjects/automation-test-store/AutoStore_Homepage_PO";
+
 describe("Adding multiple items to the basket", () => {
+  const autostore_homepage_PO = AutoStore_Homepage_PO();
   before(() => {
     cy.fixture("products").then((data) => {
       globalThis.data = data;
@@ -6,8 +9,10 @@ describe("Adding multiple items to the basket", () => {
   });
 
   beforeEach(() => {
-    cy.visit("https://automationteststore.com/");
-    cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
+    autostore_homepage_PO.navigateToHomepage();
+    autostore_homepage_PO.clickOn_HairCare_Link();
+    // cy.visit("https://automationteststore.com/");
+    // cy.get("a[href*='product/category&path=']").contains("Hair Care").click();
   });
 
   it("Add specific items to basket", () => {
