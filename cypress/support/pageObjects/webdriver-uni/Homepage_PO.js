66ccb6a3078e8152fc1f1ad4704f099b2264239c
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-19 16:29:32 +0200

    same for homepage

--- cypress/support/pageObjects/webdriver-uni/Homepage_PO.js
+++ cypress/support/pageObjects/webdriver-uni/Homepage_PO.js
@@ -1,13 +1,17 @@
 import { outsmartNewTabOpening } from "../../commands";
 
 class HomePage_PO {
+  // selectors:
+  constructor() {
+    this.contactUsLink = "#contact-us";
+  }
   // commands:
   visitHomePage() {
     cy.visit(Cypress.env("webdriveruni_homepage"));
   }
 
   clickOn_ContactUs_Button() {
-    outsmartNewTabOpening("#contact-us");
+    outsmartNewTabOpening(this.contactUsLink);
   }
 }
 
