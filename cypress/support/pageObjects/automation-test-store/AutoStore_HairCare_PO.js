45279aa0cec766e86b205e66c95634e492001d2e
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-23 15:16:22 +0200

    example of using debugger

--- cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
+++ cypress/support/pageObjects/automation-test-store/AutoStore_HairCare_PO.js
@@ -6,7 +6,13 @@ class AutoStore_HairCare_PO {
   addHairCareProductsToBasket() {
     globalThis.data.productNames.forEach(function (element) {
       cy.addProductToBasket(element);
+      //   .then(() => {
+      //   // debugger;
+      //   // this is how to use debugger with then command
+      // });
     });
+    // cy.get(this.basketEl).click().debug();
+    // it is possible to use it as a command in chain
     cy.get(this.basketEl).click();
   }
 }
