0eb2ed5fbc3ce99e19f6f90b3afe97a7a2f37763
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-24 11:09:03 +0200

    Update same-origin-policy.js
    commented these given tests

--- cypress/e2e/same-origin-policy/same-origin-policy.js
+++ cypress/e2e/same-origin-policy/same-origin-policy.js
@@ -1,26 +1,26 @@
-describe("Cypress web security", () => {
-  it.skip("Validate visiting two different domains", () => {
-    cy.visit("https://www.webdriveruniversity.com/");
-    cy.visit("https://automationteststore.com/");
-    cy.get(".welcome_msg h4")
-      .invoke("text")
-      .should("contain", "Welcome to the Automation Test Store!");
-  });
+// describe("Cypress web security", () => {
+//   it.skip("Validate visiting two different domains", () => {
+//     cy.visit("https://www.webdriveruniversity.com/");
+//     cy.visit("https://automationteststore.com/");
+//     cy.get(".welcome_msg h4")
+//       .invoke("text")
+//       .should("contain", "Welcome to the Automation Test Store!");
+//   });
 
-  it("Validate visiting two different domains via user actions", () => {
-    cy.visit("https://www.webdriveruniversity.com/");
-    cy.get("#automation-test-store").invoke("removeAttr", "target").click();
-    cy.get(".welcome_msg h4")
-      .invoke("text")
-      .should("contain", "Welcome to the Automation Test Store!");
-  });
+//   it("Validate visiting two different domains via user actions", () => {
+//     cy.visit("https://www.webdriveruniversity.com/");
+//     cy.get("#automation-test-store").invoke("removeAttr", "target").click();
+//     cy.get(".welcome_msg h4")
+//       .invoke("text")
+//       .should("contain", "Welcome to the Automation Test Store!");
+//   });
 
-  it("Different origins", () => {
-    cy.origin("automationteststore.com", () => {
-      cy.visit("/");
-    });
-    cy.origin("webdriveruniversity.com", () => {
-      cy.visit("/");
-    });
-  });
-});
+//   it("Different origins", () => {
+//     cy.origin("automationteststore.com", () => {
+//       cy.visit("/");
+//     });
+//     cy.origin("webdriveruniversity.com", () => {
+//       cy.visit("/");
+//     });
+//   });
+// });
