0714c6e577a16a480cddfd944027035fdbb29c0f
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-25 09:39:00 +0200

    partial logic implemented

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,30 @@
 const { defineConfig } = require("cypress");
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve(
+    "cypress\\config",
+    "config",
+    `${file}.json`
+  );
+
+  if (!fs.existsSync(pathToConfigFile)) {
+    console.log("No custom config file found.");
+    return {};
+  }
+
+  return fs.readJson(pathToConfigFile);
+}
 
 module.exports = defineConfig({
   projectId: "khme5n",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const file = config.env.configFile || "";
+
+      return getConfigurationByFile(file);
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
     excludeSpecPattern: "cypress/e2e/other/*.js",
