d40c98d19e66eebe3cb14f24bd45b5450b5d7113
Tomas <tomas.jurkovic@hotmail.com>
2023-10-26 14:33:24 +0200

    more examples

--- cypress/e2e/api/json-example.js
+++ cypress/e2e/api/json-example.js
@@ -5,8 +5,33 @@ describe("JSON object", () => {
       key2: "Jones",
     };
 
+    const exampleArrayOfValues = ["Sophie", "Rose", "Howard"];
+
+    const users = {
+      firstName: "Joe",
+      lastName: "Blogs",
+      age: 30,
+      students: [
+        {
+          firstName: "Jim",
+          lastName: "Blogs",
+        },
+        {
+          firstName: "Sarah",
+          lastName: "Parker",
+        },
+      ],
+    };
     // two options how to extract JSON objects
-    cy.log(exampleObject.key, exampleObject.key2);
-    cy.log(exampleObject["key"], exampleObject["key2"]);
+    cy.log(exampleObject.key, exampleObject.key2); // Tim, Jones
+    cy.log(exampleObject["key"], exampleObject["key2"]); // Tim, Jones
+
+    // extract data from array:
+    cy.log(exampleArrayOfValues[0], exampleArrayOfValues[1]); // Sophie, Rose
+
+    // extract from more complex object:
+    cy.log(users.lastName); // Blogs
+    cy.log(users.students[1].lastName); // Parker
+    cy.log(`${users.students[0].firstName} ${users.students[0].lastName}`); // Jim Blogs
   });
 });
