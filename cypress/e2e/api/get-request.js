2d931501c320c438744b90d3643a474e4a75b17c
Tomas <tomas.jurkovic@hotmail.com>
2023-10-27 13:53:47 +0200

    added more examples of validations

--- cypress/e2e/api/get-request.js
+++ cypress/e2e/api/get-request.js
@@ -8,4 +8,22 @@ describe("Get request - GET Gin Tonic open API", () => {
     );
     result.its("status").should("equal", 200);
   });
+  it("Validate correct keys and values of the /gin%20tonic API", () => {
+    cy.request({
+      method: "GET",
+      url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin%20tonic",
+      headers: {
+        accept: "application/json",
+      },
+    }).then((response) => {
+      let body = JSON.parse(JSON.stringify(response.body));
+      cy.log(body);
+      console.log(body);
+      expect(body.drinks[0].idDrink).to.eq("178365");
+      expect(body.drinks[0]).has.property("strAlcoholic", "Alcoholic");
+      expect(body.drinks[0]).has.property("strDrink", "Gin Tonic");
+      expect(body.drinks[0]).has.property("strCategory", "Cocktail");
+      expect(body.drinks).to.have.length(1); // there is only one object
+    });
+  });
 });
