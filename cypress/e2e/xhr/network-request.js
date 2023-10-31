8a37848995893b8a8b97f9552cc513060b258a2c
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-31 14:46:33 +0100

    added put request

--- cypress/e2e/xhr/network-request.js
+++ cypress/e2e/xhr/network-request.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 describe("XHR Requests", () => {
+    let message = "Unable to find comment";
     const bodyText = "Hello world";
     beforeEach(() => {
         cy.visit('https://example.cypress.io/commands/network-requests');
@@ -51,7 +52,7 @@ describe("XHR Requests", () => {
             expect(request.body).include('email');
             expect(response.body).to.have.property('name', "Using POST in cy.intercept()");
             expect(request.headers).to.have.property('content-type');
-            expect(request.headers).to.have.property('origin', 'https://example.cypress.io');
+            expect(request.headers).to.have.property("host", "jsonplaceholder.cypress.io");
         });
     });
 
@@ -79,5 +80,25 @@ describe("XHR Requests", () => {
             expect(response.body).to.have.property('name', "David Raya")
 
         });
+    });
+
+    it("PUT request mocking there is no content: 404 status code", () => {
+        cy.intercept({
+            method: "PUT",
+            url: "**/comments/*"
+        }, {
+
+            statusCode: 404,
+            body: {
+                error: message
+            },
+            delay: 500
+        }).as("putComment");
+        // intercept GET comments API with wildcards that contains '/commnets' in URL
+
+        cy.get(".network-put").click();
+        cy.wait("@putComment").its("response.statusCode").should('eq', 404);
+        cy.get('.network-put-comment').invoke('text').should('eq', message)
+
     });
 });
