1298796adb11e7dfa3aa2f3fc5e647d2e8ae51ca
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-31 11:34:44 +0100

    progress so far

---
+++ cypress/e2e/xhr/network-request.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+describe("Put Request", () => {
+
+    beforeEach(() => {
+        cy.visit('https://example.cypress.io/commands/network-requests');
+    });
+
+    it("GET request", () => {
+        cy.click()
+    });
+});
