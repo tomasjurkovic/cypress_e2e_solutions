6356efadb75ffaa49baf7e4beead699f93215d4e
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-31 10:41:31 +0100

    PUT request example created

--- cypress/e2e/api/post-request.js
+++ cypress/e2e/api/post-request.js
@@ -3,35 +3,35 @@
 describe("Post Request", () => {
   const titleOfPosts = new Array();
   let randomTitle = Math.random().toString(36).substring(1) + Math.random().toString(36).substring(1);
-  
+
   it("Create a new post via /posts api", () => {
     cy.request({
       method: "POST",
       url: "http://localhost:3000/posts",
       body: {
-          title: randomTitle,
-          author: "Sarah Jones"
+        title: randomTitle,
+        author: "Sarah Jones"
       }
     }).then(response => {
-        expect(response.status).to.eql(201)
+      expect(response.status).to.eql(201)
     })
   });
-  
+
   it("Validate title of latest post", () => {
     cy.request({
-        method: "GET",
-        url: "http://localhost:3000/posts",
-        headers: {
-            accept: "application/json"
-        }
+      method: "GET",
+      url: "http://localhost:3000/posts",
+      headers: {
+        accept: "application/json"
+      }
     }).then(response => {
-        let body = JSON.parse(JSON.stringify(response.body));
-        body.forEach(function(item) {
-            titleOfPosts.push(item["title"]);
-        })
+      let body = JSON.parse(JSON.stringify(response.body));
+      body.forEach(function (item) {
+        titleOfPosts.push(item["title"]);
+      })
     }).then(() => {
-        const latestPost = titleOfPosts[titleOfPosts.length -1]
-        expect(latestPost).to.eq(randomTitle);
+      const latestPost = titleOfPosts[titleOfPosts.length - 1]
+      expect(latestPost).to.eq(randomTitle);
     })
   })
 });
