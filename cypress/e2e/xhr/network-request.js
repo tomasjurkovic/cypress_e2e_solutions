d1f3ef6b5c7d636457de7839176e35902425f387
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-31 13:54:28 +0100

    added specific test which uses mocked API and verifies if mocked value is used in page

--- cypress/e2e/xhr/network-request.js
+++ cypress/e2e/xhr/network-request.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Put Request", () => {
-
+    const bodyText = "Hello world";
     beforeEach(() => {
         cy.visit('https://example.cypress.io/commands/network-requests');
     });
@@ -14,6 +14,27 @@ describe("Put Request", () => {
         // intercept GET comments API with wildcards that contains '/commnets/' in URL
 
         cy.get(".network-btn").click();
-        cy.wait("@getComment").its("response.statusCode").should('eq', 200)
+        cy.wait("@getComment").its("response.statusCode").should('eq', 200);
+    });
+
+    it("GET request with mocked response", () => {
+        cy.intercept({
+            method: "GET",
+            url: "**/comments/*"
+        },
+            {
+                body: {
+                    postId: 1,
+                    id: 1,
+                    name: "Test name 123",
+                    email: 'joe.blogs@gmail.com',
+                    body: bodyText
+                }
+            }).as("getComment");
+        // intercept GET comments API with wildcards that contains '/commnets/' in URL
+
+        cy.get(".network-btn").click();
+        cy.wait("@getComment").its("response.statusCode").should('eq', 200);
+        cy.get('.network-comment').invoke('text').should('contain', bodyText);
     });
 });
