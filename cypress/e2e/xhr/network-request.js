f0896764aa9c58d434f08bca298923619cd1e022
Tomas Jurkovic <tomas.jurkovic@hotmail.com>
2023-10-31 14:29:44 +0100

    added post requests

--- cypress/e2e/xhr/network-request.js
+++ cypress/e2e/xhr/network-request.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("Put Request", () => {
+describe("XHR Requests", () => {
     const bodyText = "Hello world";
     beforeEach(() => {
         cy.visit('https://example.cypress.io/commands/network-requests');
@@ -36,5 +36,48 @@ describe("Put Request", () => {
         cy.get(".network-btn").click();
         cy.wait("@getComment").its("response.statusCode").should('eq', 200);
         cy.get('.network-comment').invoke('text').should('contain', bodyText);
+    });
+
+    it("POST request", () => {
+        cy.intercept({
+            method: "POST",
+            url: "**/comments"
+        }).as("postComment");
+        // intercept GET comments API with wildcards that contains '/commnets' in URL
+
+        cy.get(".network-post").click();
+        // cy.wait("@postComment").its("response.statusCode").should('eq', 200);
+        cy.wait("@postComment").should(({request, response}) => {
+            expect(request.body).include('email');
+            expect(response.body).to.have.property('name', "Using POST in cy.intercept()");
+            expect(request.headers).to.have.property('content-type');
+            expect(request.headers).to.have.property('origin', 'https://example.cypress.io');
+        });
+    });
+
+    it("POST request with mocked response ", () => {
+        cy.intercept({
+            method: "POST",
+            url: "**/comments"
+        }, {
+            body: {
+                name: "David Raya",
+                email: "david.raya@arsenal.com",
+                body: "First post done this way",
+                id: 666
+            }
+        }).as("postComment");
+        // intercept GET comments API with wildcards that contains '/commnets' in URL
+
+        cy.get(".network-post").click();
+        // cy.wait("@postComment").its("response.statusCode").should('eq', 200);
+        cy.wait("@postComment").should(({request, response}) => {
+            console.log(request);
+            console.log(response);
+
+            expect(request.body).include('email');
+            expect(response.body).to.have.property('name', "David Raya")
+
+        });
     });
 });
